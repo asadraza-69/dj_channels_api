d9feab39ff5fdcb5ff744c7fa5686b558b263e59
asadraza-69 <121610747+asadraza-69@users.noreply.github.com>
2024-04-01 15:18:23 +0500

    Js function Of Websocket Plus sync Aync difference

--- channel_app/consumer.py
+++ channel_app/consumer.py
@@ -1,4 +1,8 @@
+import asyncio
+from time import sleep
+
 from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
 
 
 class MySyncConsumer(SyncConsumer):
@@ -13,13 +17,17 @@ class MySyncConsumer(SyncConsumer):
 
     # This handler is called when data is received from the client.
     def websocket_receive(self, event):
-        print('Messaged Received...',event.get('text'))
+        print('Messaged Received...', event.get('text'))
         print('--------------------------------')
+        for i in range(50):
+            self.send(dict(type='websocket.send', text=f'Hello World = {i}'))
+            sleep(1)
 
     # This handler is called when the connection to the client is lost,
     # either from the client closing the connection or other reasons.
     def websocket_disconnect(self, event):
         print('Websocket Disconnected...')
+        raise StopConsumer
 
 
 class MyAsyncConsumer(AsyncConsumer):
@@ -27,12 +35,22 @@ class MyAsyncConsumer(AsyncConsumer):
     # and is about to finish the WebSocket handshake.
     async def websocket_connect(self, event):
         print('WebSocket Connect...')
+        await self.send({
+            'type': 'websocket.accept',
+            'accept': True,
+        })
 
     # This handler is called when data is received from the client.
     async def websocket_receive(self, event):
-        print('Websocket Received...')
+        print('Message received from Client', event)
+        print('Message received from Client', event['text'])
+        for i in range(50):
+            await self.send(dict(type='websocket.send', text=f'Hello World = {i}'))
+
+            await asyncio.sleep(1)
 
     # This handler is called when the connection to the client is lost,
     # either from the client closing the connection or other reasons.
     async def websocket_disconnect(self, event):
         print('Websocket Disconnect...')
+        raise StopConsumer
--- channel_app/views.py
+++ channel_app/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import render
+
+
 # Create your views here.
 
 
 def index(request):
     return JsonResponse({"Status": True})
+
+
+def welcome(request):
+    return render(request, 'channel_app/index.html', {})
